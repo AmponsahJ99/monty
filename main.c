773df4c7c8ef1337918611d2c8336fd8ac0223ae
AmponsahJ99 <amjust99@gmail.com>
2023-08-28 13:56:35 -0700

    alx monty program

--- main.c
+++ main.c
@@ -34,13 +34,9 @@ int main(int argc, char *argv[])
 		read_line = getline(&content, &size, file);
 		bus.content = content;
 		counter++;
-		if (read_line > 0)
-		{
-			execute(content, &stack, counter, file);
-		}
-		free(content);
 	}
 	free_stack(stack);
 	fclose(file);
-return (0);
+	
+	return (0);
 }
